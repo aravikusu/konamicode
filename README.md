4df40bdf1ae149b3b382f008f2e129325f4651e0
Aravix <49846328+aravikusu@users.noreply.github.com>
2025-02-06 13:47:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# KonamiCode for Godot
+<p align="center">
+    <img src="addons/konamicode/icon.svg" alt="Logo" width="200">
+    <br />
+    <i>Up, Up, Down, Down...</i>
+</p>
+
+
+A very simple addon that adds a new Node that listens for a string of key combinations.
+
+I'm the type of person that loves putting random cryptic secrets in my projects, so I find myself using something like this often. I figured I'd share it with the community as someone might have a use for it.
+
+## Using it
+KonamiCode comes with a single new Node: KonamiCode. The exported variables all have pretty self-explanatory names, but just to cover my bases:
+
+You can either set your own code in the exported `code_override` variable, or just use the default, which is the traditional Konami Code.
+
+When that's done, set up if it should be active by default with the `active` prop, and if it should only fire once with the `fire_only_once` prop.
+
+And finally, just connect the `success` signal!
+
+Easy peasy. Now add your own secrets (or just use it for other key-combination related things)!
---
+++ addons/konamicode/konamicode.gd
@@ -0,0 +1,65 @@
+extends Node
+signal success
+
+## Will it do the thing?
+@export var active: bool = true
+
+## What you want your code to be.
+##[br]If left empty, it defaults to the Konami Code.
+@export var code_override: Array[Key]
+
+## If enabled the listener will disable itself
+## once successfully triggered.
+@export var fire_only_once: bool = false
+
+## The default, the classic.
+const KONAMI_CODE: Array[Key] = [
+	KEY_UP, KEY_UP, KEY_DOWN, KEY_DOWN,
+	KEY_LEFT, KEY_RIGHT, KEY_LEFT, KEY_RIGHT,
+	KEY_A, KEY_B, KEY_ENTER 
+]
+
+var _relevant_code: Array[Key]
+var _successful_presses: int = 0
+
+func _ready() -> void:
+	if len(code_override) > 0:
+		_relevant_code = code_override
+	else:
+		_relevant_code = KONAMI_CODE
+
+func _input(event: InputEvent) -> void:
+	if !active:
+		return
+	
+	if event is not InputEventKey:
+		return
+	
+	if event.echo || !event.pressed:
+		return
+
+	_code_checker(event)
+
+func _code_checker(event: InputEventKey) -> void:
+	if _relevant_code[_successful_presses] == event.keycode:
+		print(event)
+		_successful_presses += 1
+	else:
+		_successful_presses = 0
+	
+	if len(_relevant_code) == _successful_presses:
+		success.emit()
+		_successful_presses = 0
+
+		if fire_only_once:
+			active = false
+
+## Below are functions you can use to enable/disable
+## the node, if you're the type that prefers doing
+## that instead of going on properties directly.
+
+func enable() -> void:
+	active = true
+
+func disable() -> void:
+	active = false
